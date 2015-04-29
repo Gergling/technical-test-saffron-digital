d438fa54179cda202706df0fb3cf5eefa0c3b06b
gergling <gregmichaeldavies@gmail.com>
2015-04-30 00:02:10 +0100

    Controller set up to use the window service.

--- src/public/module/application/controller/index.js
+++ src/public/module/application/controller/index.js
@@ -1,11 +1,27 @@
 angular.module('application').controller("application.controller.index", [
 
     "$rootScope",
+    "application.service.window",
 
-    function ($scope) {
+    function ($scope, windows) {
 
         "use strict";
 
-        $scope.windows = [ ];
+        var field = function (name) {
+            return {
+                name: name,
+                label: angular.uppercase(name.charAt(0)) + name.substring(1)
+            };
+        };
+
+        $scope.fields = [
+            field("name"),
+            field("start"),
+            field("end")
+        ];
+
+        windows.list().then(function (response) {
+            $scope.windows = response;
+        });
     }
 ]);
